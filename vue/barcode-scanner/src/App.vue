b0062d60fe234fce36bf004a4df7674bae3263a3
xulihang <xulihanghai@163.com>
2022-06-29 17:54:51 +0800

    add vue

---
+++ vue/barcode-scanner/src/App.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <camera-preview
+    desired-camera="founder"
+  >
+  </camera-preview>
+</template>
+
+<style>
+  camera-preview {
+    height:100%;
+    width:100%;
+    position: absolute;
+    right:0;
+    top:0;
+  }
+</style>
