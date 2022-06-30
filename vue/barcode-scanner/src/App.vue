e0d13e4fefb19ac446d00b5a4f9ac64a38341d8e
xulihang <xulihanghai@163.com>
2022-06-30 16:58:06 +0800

    add buttons for vue demo

--- vue/barcode-scanner/src/App.vue
+++ vue/barcode-scanner/src/App.vue
@@ -1,11 +1,27 @@
 <script setup lang="ts">
+  import { ref } from 'vue';
 
+  const active = ref(false);
+  const startCamera = () => {
+    active.value  = true;
+  }
+
+  const stopCamera = () => {
+    active.value  = false;
+  }
 </script>
 
 <template>
-  <camera-preview
+  <div :style="{display: active ? 'none' : '' }">
+    <button v-on:click="startCamera">Start Camera</button>
+  </div>
+  <camera-preview 
     desired-camera="founder"
+    draw-overlay="true"
+    :active="active"
+    :style="{display: active ? '' : 'none' }"
   >
+    <button id="close-btn" v-on:click="stopCamera">Close</button>
   </camera-preview>
 </template>
 
