3ea8eb37df96a104ec5fd93dba205d1ee43d0a93
xulihang <xulihanghai@163.com>
2022-06-30 17:21:59 +0800

    update App.vue to use the barcode reader

--- vue/barcode-scanner/src/App.vue
+++ vue/barcode-scanner/src/App.vue
@@ -1,13 +1,81 @@
 <script setup lang="ts">
-  import { ref } from 'vue';
+  import { onMounted, ref } from 'vue';
+  import { BarcodeReader, type TextResult } from "dynamsoft-javascript-barcode";
 
+  BarcodeReader.engineResourcePath = "https://unpkg.com/dynamsoft-javascript-barcode@9.0.2/dist/";
+  let reader:BarcodeReader;
+  let scanned = false;
+  let decoding = false;
+  let interval:any = null;
+  const cameraElement:any = ref(null);
   const active = ref(false);
+
+  onMounted(async () => {
+    reader = await BarcodeReader.createInstance();
+  })
+
   const startCamera = () => {
     active.value  = true;
   }
 
   const stopCamera = () => {
     active.value  = false;
+  }
+
+  const onOpened = () => {
+    console.log("opened");
+    startDecoding();
+  }
+
+  const startDecoding = () => {
+    scanned = false;
+    stopDecoding();
+    //1000/25=40
+    interval = setInterval(decode, 40);
+  }
+
+  const stopDecoding = () => {
+    clearInterval(interval);
+  }
+
+  const decode = async () => {
+    if (decoding === false && reader && cameraElement.value) {
+      console.log("decoding");
+      const video = await cameraElement.value.getVideoElement();
+      decoding = true;
+      try {
+        let results = await reader.decode(video);
+        if (results.length>0 && scanned === false) {
+          scanned = true;
+          stopDecoding();
+          stopCamera();
+          alert(results[0].barcodeText);
+        }else{
+          await cameraElement.value.updateAnalysingResults(wrapResults(results));
+        }
+
+      } catch (error) {
+        console.log(error);
+      }
+      decoding = false;
+    }
+  }
+
+  const wrapResults = (results:TextResult[]) => {
+    let analysingResults = [];
+    for (let index = 0; index < results.length; index++) {
+      const result = results[index];
+      let analysingResult:any = {};
+      analysingResult.text = result.barcodeText;
+      analysingResult.localizationResult = [
+        {x:result.localizationResult.x1,y:result.localizationResult.y1},
+        {x:result.localizationResult.x2,y:result.localizationResult.y2},
+        {x:result.localizationResult.x3,y:result.localizationResult.y3},
+        {x:result.localizationResult.x4,y:result.localizationResult.y4}
+      ];
+      analysingResults.push(analysingResult)
+    }
+    return analysingResults;
   }
 </script>
 
@@ -16,8 +84,10 @@
     <button v-on:click="startCamera">Start Camera</button>
   </div>
   <camera-preview 
+    ref="cameraElement"
     desired-camera="founder"
     draw-overlay="true"
+    v-on:opened="onOpened"
     :active="active"
     :style="{display: active ? '' : 'none' }"
   >
